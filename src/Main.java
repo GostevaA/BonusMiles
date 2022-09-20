769ddcf918ad814fbaeeffba3ea23b6c21b7979b
Anna Gosteva <gostevaay@yandex.ru>
2022-09-20 14:09:00 +0300

    new files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 4500; // стоимость билета
+        int twentyMiles = 20; //
+        int bonusMiles = price / twentyMiles;
+        System.out.println("Бонусные мили: " + bonusMiles);
+
+    }
+}
